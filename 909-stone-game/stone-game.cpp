888b7102e3c42167d374a959065b84ad449f9b36
Dharmesh Bhadrecha <bhadrechadharmesh141@gmail.com>
2026-08-03 12:50:23 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (98.58%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -16,9 +16,6 @@ class Solution {
 
 public:
     bool stoneGame(vector<int>& piles) {
-        int n = piles.size();
-        dp.assign(n, vector<int>(n,-1));
-
-        return helper(piles,0,n-1) > 0;
+        return true;
     }
 };
