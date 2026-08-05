cd95944e50d2dda11095b2725ba47a684d74f9cc
Dharmesh Bhadrecha <bhadrechadharmesh141@gmail.com>
2026-08-05 18:31:23 +0530

    Time: 318 ms (40.69%) | Memory: 344 MB (34.14%) - LeetSync

--- 3561-remove-methods-from-project/remove-methods-from-project.cpp
+++ 3561-remove-methods-from-project/remove-methods-from-project.cpp
@@ -1,51 +1,45 @@
 class Solution {
-    void dfsSus(unordered_map<int,vector<int>>& graph,int node,vector<int>&vis){
+    void dfsSus(unordered_map<int,vector<int>>& graph, int node, vector<int>& vis){
         vis[node] = -1;
 
-        for(auto it:graph[node]){
-            if(vis[it]==0){
-                dfsSus(graph,it,vis);
+        for(auto it : graph[node]){
+            if(vis[it] == 0){
+                dfsSus(graph, it, vis);
             }
         }
     }
 
-    void dfs(unordered_map<int,vector<int>>& graph,int node,vector<int>& vis,bool &isValid){
-        if(vis[node]==-1){
-            isValid = true;
-            return;
-        }
-        vis[node] = 2;
-
-
-        for(auto it:graph[node]){
-            if(vis[it]!=2){
-                dfs(graph,it,vis,isValid);
-            }
-        }
-    }
 public:
     vector<int> remainingMethods(int n, int k, vector<vector<int>>& arr) {
         unordered_map<int,vector<int>> graph;
 
-        for(int i=0;i<arr.size();i++){
-            graph[arr[i][0]].push_back(arr[i][1]);
+        for(auto &e : arr){
+            graph[e[0]].push_back(e[1]);
         }
 
         vector<int> vis(n,0);
 
+        // Mark suspicious methods
         dfsSus(graph,k,vis);
 
-        bool isValid = false;
-        for(int i=0;i<n;i++){
-            if(isValid)break;
-            if(vis[i]==0 && !isValid){
-                dfs(graph,i,vis,isValid);
+        // Check dependency from normal -> suspicious
+        for(auto &e : arr){
+            int u = e[0];
+            int v = e[1];
+
+            if(vis[u] == 0 && vis[v] == -1){
+                vector<int> ans;
+                for(int i=0;i<n;i++){
+                    ans.push_back(i);
+                }
+                return ans;
             }
         }
 
+        // Remove suspicious methods
         vector<int> ans;
         for(int i=0;i<n;i++){
-            if(vis[i]==2 || isValid){
+            if(vis[i] == 0){
                 ans.push_back(i);
             }
         }
